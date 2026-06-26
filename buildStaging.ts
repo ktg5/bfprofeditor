0b6816e2ddc761de5b075dc3a7063fee8b87343c
kyle <42522603+ktg5@users.noreply.github.com>
2026-06-25 18:12:34 -0700

    i'm prob not gonna use this so imma just remove it

--- buildStaging.ts
+++ buildStaging.ts
@@ -7,6 +7,7 @@ import config from "./config.json";
 
 
 const __filename = fileURLToPath(import.meta.url);
+// @ts-ignore
 const __dirname = path.dirname(__filename);
 
 
@@ -68,59 +69,12 @@ async function copyDir(sourceDir: string, newDir: string):Promise<void> {
 
 
 export async function buildStaging():Promise<void> {
-    return new Promise<void>((resolve) => {
-        function replaceMatches(match: string, tag: string) {
-            switch (tag) {
-                // nav & footer
-                case "nav":
-                    return fs.readFileSync('./html/defs/nav.html', { encoding: 'utf-8' });
-                case "footer":
-                    return fs.readFileSync('./html/defs/footer.html', { encoding: 'utf-8' });
-        
-                    
-                // default error message
-                default:
-                    return `<kbd>buildStaging.ts:replaceMatches :: unknown match: ${match}</kbd>`;
-            }
-        }
-        
-        
-        // for replacing HTML stuff with our own
-        function replaceHtml(dir: string, base = dir):Promise<void> {
-            return new Promise<void>((resolve) => {
-                for (const file of fs.readdirSync(dir)) {
-                    const fullPath = path.join(dir, file);
-                    const stat = fs.statSync(fullPath);
-            
-                    // run in dir if is a dir
-                    // + ignore defaults dir
-                    if (
-                        stat.isDirectory()
-                        && file !== "defs"
-                    ) replaceHtml(fullPath, base);
-                    else if (file.endsWith('.html')) {
-                        const fileText = fs.readFileSync(fullPath, { encoding: 'utf-8' });
-                        const output = fileText.replaceAll(/<\*--\s*(.*?)\s*--\*>/g, replaceMatches);
-                        fs.writeFileSync(fullPath, output, { encoding: 'utf-8' });
-                    }
-                }
-                resolve();
-            });
-        };
-        
-        
+    return new Promise<void>((resolve) => {      
         // copy current html folder
         if (fs.existsSync(config.stagingHtmlDir)) rmStaging();
         console.log(`🔃 copying current "${config.baseHtmlDir}" folder to a "${config.stagingHtmlDir}" for Vite to compile...`);
         copyDir(config.baseHtmlDir, config.stagingHtmlDir).then(async () => {
             console.log('✅ copy complete!');
-        
-            // replace the html within the staging folder
-            console.log('🔃 looking for & replacing all lines that want some html...');
-            await replaceHtml(path.resolve(__dirname, config.stagingHtmlDir));
-
-            // end
-            console.log('✅ replaced html lines!');
             resolve();
         });
     });
